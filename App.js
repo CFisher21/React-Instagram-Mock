beb46135f3a9922ba13484ae1620a0ca225805aa
Cody Fisher <95709270+CFisher21@users.noreply.github.com>
2022-09-19 23:28:14 -0230

    Add files via upload

---
+++ App.js
@@ -0,0 +1,37 @@
+import {BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
+import Switch from 'react-switch';
+import Login from './pages/login';
+import Feed from './pages/feed';
+import Profile from './pages/profile';
+import './pages/App.css'
+import Feedicon from './Nav icons/FeedIcon.png'
+import Profileicon from './Nav icons/ProfileIcon.png'
+import { useState } from 'react';
+
+function App() {
+    
+    return (
+        <>
+            
+                
+            <nav className='nav-container'>
+            <h1 className='finstagramNav'>Finstagram</h1>
+                <div className='nav-control'>
+                <Link to="/Feed" className='linkCenter'><img src={Feedicon} alt='123' className='navImg'></img></Link>
+                <Link to="/Profile" className='linkCenter'><img src={Profileicon} alt='123' className='navImg'></img></Link>
+                <Link to="/Login" className='linkCenter' id='loginButton2'><button className='loginButton3'>Login</button></Link>
+                </div>
+            </nav>
+
+          <Routes>
+              <Route path="/Login" element={<Login ></Login>}></Route>
+              <Route path='/Feed' element={<Feed ></Feed>}></Route>
+              <Route path='/Profile' element={<Profile></Profile>}></Route>
+          </Routes>
+      </>  
+    )
+}
+
+export default App
+
+
