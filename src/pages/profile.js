85bdb2818a1e64307d52e17aa76a8006d306941d
Cody Fisher <95709270+CFisher21@users.noreply.github.com>
2023-01-04 15:55:24 -0330

    Updated to remove weak warnings from the console.

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -9,32 +9,37 @@ import Stars from './Profile/Stars.jpg'
 import Sunset from './Profile/sunset.jpg'
 import Thinking from './Profile/Thinking.jpg'
 import Trees from './Profile/Trees.jpg'
+import { useContext } from 'react'
+import { userContext } from './userContext'
 
 
 
 
 function Profile() {
+
+    const user = useContext(userContext);
+
     return(
         <div className='profile-container'>
-        <img src={ProfilePhoto} className="profileImg"></img>
+        <img src={ProfilePhoto} className="profileImg" alt=''></img>
         <div className='info'>
-        <h1 className='profileUsername'>Justanotheruser</h1>
+        <h1 className='profileUsername'>{user}</h1>
         <p className='followers'>Followers 12</p>
         <p className='following'>Following 15</p>
         <p className='profileName'>Justin T.</p>
         <p className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed libero varius, pharetra lacus ac, aliquam sem. Ut pulvinar urna ex, rhoncus sodales ligula fermentum sed. Sed congue sit amet libero ut euismod. Sed vel porttitor augue. Praesent iaculis ex vel leo bibendum tempus. Proin nunc nibh, bibendum nec lacinia.</p>
         </div>
         <div className='photoBox'>
-            <img src={BeachSunset} className="gallery"></img>
-            <img src={Birds} className="gallery"></img>
-            <img src={Blue} className="gallery"></img>
-            <img src={Boat} className="gallery"></img>
-            <img src={Crossing} className="gallery"></img>
-            <img src={Mountian} className="gallery"></img>
-            <img src={Stars} className="gallery"></img>
-            <img src={Sunset} className="gallery"></img>
-            <img src={Thinking}className="gallery"></img>
-            <img src={Trees}className="gallery"></img>
+            <img src={BeachSunset} className="gallery" alt=''></img>
+            <img src={Birds} className="gallery" alt=''></img>
+            <img src={Blue} className="gallery" alt=''></img>
+            <img src={Boat} className="gallery" alt=''></img>
+            <img src={Crossing} className="gallery" alt=''></img>
+            <img src={Mountian} className="gallery" alt=''></img>
+            <img src={Stars} className="gallery" alt=''></img>
+            <img src={Sunset} className="gallery" alt=''></img>
+            <img src={Thinking}className="gallery" alt=''></img>
+            <img src={Trees}className="gallery" alt=''></img>
             
         </div>
         </div>
